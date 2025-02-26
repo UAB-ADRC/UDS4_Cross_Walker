4039153f0a40847c340e7706d52e091e8f0cc2b1
Murchison <cfmurch@uab.edu>
2025-02-26 11:17:25 -0600

    Commit for NACC upload for sharing

--- README.md
+++ README.md
@@ -1,42 +1,74 @@
 # JSON Script for Data Processing
-## 1. Json_script.py
-This Python script processes Excel files in a given folder, extracts relevant data, and saves the output as JSON. It's designed to help automate the conversion of data elements between UDS3 and UDS4 formats.
+
+## Authors
+- [Chad Murchison](https://github.com/cfmurch), UAB ADRC
+- [Jai Nagidi](https://github.com/jnagidi), UAB ADRC
+
+## License
+This project is licensed under the MIT License - see [LICENSE](LICENSE) for details
+
+## UDS_JSON_Maker.py
+This is the primary Python script which processes Excel files in a given folder (given as a command line argument), process the relevant mapping rules, and saves the output as JSON.<br>
+<br>
+The goal of the JSON files is to help programmatically apply the conversion of data elements between UDS3 and UDS4 formats.  An emphasis is given on UDS3 -> UDS4 although reversible rules are also flagged.
+
+## Crosswalk Folder
+This folder contains properly formatted Excel workbooks used by the Python script to generate the JSON files containing the following sheets:
+
+  - UDS3_4 Tables Data Dictionary: Data dictionary for the three UDS variable tables and the change indicators between UDS3 and UDS4
+  - XX UDS3 DED: Form specific table for UDS3 built from the Data Element Dictionary for UDS3, included for reference
+  - XX UDS3 REDCap: Form specific table for UDS3 built from the original Kansas ADRC REDCap data dictionary
+  - XX UDS4 REDCap: Form specific table for UDS4
+    - Note, Template Mappings are oriented using REDCap-to-REDCap conventions
+  - Mappings Data Dictionary: Data dictionary for the template mappings to migrate UDS variables from version 3 to version 4
+  - Template Mappings REDCap: The sheet containing all rules processed and stored into JSON format.  Modifications to rules for a specific ADRC can be applied here.
+  - XX Mappings - ADRC Decisions: Ambiguous rules that should be vetted by the ADRC.  Options are flagged as ADRC Decision_A and ADRC Decision_B where applicable to indicate rule sets.
+
+<br>
+**Rules in 'Mappings - ADRC Decisions' must be vetted and added to the 'Template Mappings REDCap' sheet before compiling JSON files!**
+
+## Modification of Template Mappings
+Template mappings can be modified as needed to reflect your specific instance of UDS3 and UDS4.  Structure on the Template Mappings REDCap should remain consistent, simply update the entries in the 'UDS3 value' and 'UDS4 value' columns.<br>
+<br>
+As described, be sure to select rules from the ADRC Decision sheet and add them to the Template Mappings prior to generating JSON files.
+
+## Application
 
 ### Requirements
-
 Before you run the script, make sure you have Python installed on your system along with the required libraries. To install the necessary dependencies, follow these steps:
 
 1. Install Python (if not already installed):
    - Download Python: https://www.python.org/downloads/
    - Ensure that Python and `pip` (Python's package installer) are added to your system's PATH.
 
-2. Install required libraries by running:
+2. Install required libraries from the command line using pip (or controlling environment e.g. conda)
    ```bash
    pip install -r requirements.txt
     ```
 
 ### How to Use the Script
 Step 1: Prepare Your Files
-Ensure that your Excel files are located in a folder on your computer. These files should have multiple sheets containing data in a specific structure.
+Ensure that your Excel files are located in a folder on your computer. Default
 
 Step 2: Run the Script
-To run the script, open a terminal (or command prompt) and navigate to the folder where the script is located. Then, run the following command, replacing <folder_path> with the path to the folder containing your Excel files:
+To run the script, open your terminal or command prompt and navigate to the folder where the script is located. Run the following command, replacing <folder_path> with the path to the folder containing your Excel files if needed:
  ```bash
-python Json_script.py <folder_path>
+python UDS_JSON_Maker.py <folder_path>
  ```
 For example:
  ```bash
-python Json_script.py C:/Users/YourUsername/Downloads/crosswalks
+python UDS_JSON_Maker.py C:/Users/YourUsername/Downloads/crosswalks
  ```
+If no folder path is provided, this will default to './Crosswalk' located in folder from which the UDS_JSON_Maker.py is run.  For convenience, a collection of crosswalk sheets are provided here.
 
 The script will process all Excel files in the provided folder and generate:
-- A JSON file for each Excel file, named <original_filename>_mappings.json.
+- A JSON file for each Excel file, named <original_filename>_mappings.json which can then be used for a programmatic crosswalk.
 
 Step 3: **Check the Output**: 
 After running the script, the generated JSON files are in the same folder as the Excel files.
 
-**Note**: Ensure that the Excel files contain the required content in Sheet 3 and Sheet 5. Some files like A1a, A4a, etc., which do not have the Template Mappings REDCap sheet. When providing the folder path as an argument, please make sure to remove or delete those Excel crosswalk files from the folder before running the script.
+**Note**: Ensure the Excel files contain the required input in the UDS3/4 REDCap dictionary sheets and Template Mappings sheets.  Note, new UDS4 forms have no mappings rules thus some crosswalk files for forms like A1a, A4a, etc., do not have the 'Template Mappings REDCap' sheet . Be sure to remove or delete those Excel crosswalk files from the folder before running the script.
 
 ## Troubleshooting
-If you encounter any issues, ensure that you are using the correct Python version and have installed all dependencies from the requirements.txt file.
-Make sure that the folder path provided contains only .xlsx files that match the required format.
+If you encounter any issues, ensure that you are using the correct Python version and have installed all dependencies from the requirements.txt file.<br>
+Make sure that the folder path provided contains only .xlsx files that match the required format described in the Crosswalk section above.
---
+++ UDS_JSON_Maker.py
@@ -0,0 +1,190 @@
+import os,sys
+import pandas as pd
+import json
+import re
+from datetime import datetime
+
+
+# Define the folder containing the files, default to ./Crosswalk
+if len(sys.argv) > 1 and sys.argv[1].strip():
+    folder_path = sys.argv[1]  # Use the provided argument
+else:
+    folder_path = "./Crosswalk" 
+
+
+## FUNCTION DEFINITIONS
+
+# Converts datetime to an ISO 8601 string
+def json_serializer(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()  
+    raise TypeError(f"Type {type(obj)} not serializable")
+
+
+# Clean text fields in the JSON structres
+def clean_text(obj):
+    " Recursively cleaning text fields in JSON structure. "
+    if isinstance(obj, dict):
+        return {k: clean_text(v) for k, v in obj.items()}  # Key-value cleaning in a dictionary
+    elif isinstance(obj, list):
+        return [clean_text(i) for i in obj]  # Text cleaning for single list items
+    elif isinstance(obj, str):
+        return obj.replace('_x000D_', '').strip()  # Remove unwanted characters and whitespace
+    return obj
+
+
+# Classification of the columns in the template mappings for JSON processing
+def classify_variable(df):
+    def classify(group):
+      # Direct mapping in UDS4/UDS3 column names are the same
+        if group['uds4 data element'].str.endswith('X').all() and group['uds3 data element'].str.endswith('X').all() and group['data type change'].eq('No').all():
+            return 'direct'
+      # Special structured mappings and mergings
+        if group['special mapping'].isin(['Structured', 'Merging', 'Merging; Calculated field']).any():
+            return 'sp'
+      # Conditional consistency for non-mergings on calculated fields
+        elif group['special mapping'].isin(['Calculated field']).any():
+            return 'cc'
+      # Special mappings catches using conformity
+        elif group['special mapping'].isna().all():
+          # If conformity or data type change exists, use conditional consistency
+            if group['conformity change'].eq('Yes').any() or group['data type change'].eq('Yes').any() or group['change in form'].eq('Yes').any():
+                return 'cc'
+          # If conformity change not in place for special mapping (e.g. free text fields) return direct mappings
+            elif group['uds4 data element'].iloc[0] == group['uds3 data element'].iloc[0]:
+                return 'direct'
+            else:
+                return 'cc'
+      # In all other cases, consider it as complex
+        else:
+            return 'complex'
+
+    # Organize by UDS4 name, apply the classification grouping, and annotate the dataframe with new 'class' column
+    # Doing hard selection of column names to make sure the grouping column gets passed in pandas 3+
+    all_columns = df.columns.tolist()
+    classifications = df.groupby('uds4 data element')[all_columns].apply(classify)
+    df = df.merge(classifications.rename('class'), on='uds4 data element', how='left')
+    return df
+
+
+# Function to categorize variables
+def categorize_variables(df):
+  # Initialize arrays by classification type
+    direct_mappings = []
+    conditional_consistency_mappings = []
+    structured_transformation_mappings = []
+    complex_mappings = []
+    
+    # For each UDS3 and UDS4 variable pair (allows for merging)
+    for (uds3_var, uds4_var), group in df.groupby(['uds3 data element', 'uds4 data element']):
+      
+      # Check class of the mapping 
+        variable_class = group['class'].iloc[0]
+      # Build common single mapping elements (UDS3 and UDS4 value, reversible from 3 to 4, any notes)
+        crosswalk_mappings = [
+            {
+                "mapping_type": row["mapping type"],
+                "mappings": [
+                    {
+                        "UDS3_value": row["uds3 value"],
+                        "UDS4_value": row["uds4 value"],
+                        "reversible": row["reversible to uds3"],
+                        "note": row.get("notes and discussion points", ""),
+                    }
+                ],
+            }
+          # Use iterrows to build and append this dictionary for each row
+            for _, row in group.iterrows()
+        ]
+
+      # Nest the mapping dictionary within the UDS3/UDS4 pairing
+        mapping_entry = {
+            "UDS3_variable": uds3_var,
+            "UDS4_variable": uds4_var,
+            "crosswalk_mappings": crosswalk_mappings,
+        }
+
+      # Organize the built JSON pairing by the four classifications
+        if variable_class == "sp":
+            structured_transformation_mappings.append(mapping_entry)
+        elif variable_class == "cc":
+            conditional_consistency_mappings.append(mapping_entry)
+        elif variable_class == "complex":
+            complex_mappings.append(mapping_entry)
+        elif variable_class == "direct":
+                direct_mappings.append({
+                "UDS3_variable": uds3_var,
+                "UDS4_variable": uds4_var,
+                "crosswalk_remappings": crosswalk_mappings,
+            })
+            
+  # Final aggregation
+    all_mappings = {
+        "Direct_Mappings": direct_mappings,
+        "Conditional_Consistency": conditional_consistency_mappings,
+        "Structured_Transformations": structured_transformation_mappings,
+        "High_Complexity": complex_mappings,
+    }
+    return all_mappings
+
+
+
+## PRIMARY LOOP
+
+# Iterate over each UDS4 file in the folder from folder_path
+for file_name in os.listdir(folder_path):
+  
+  # Process file name
+    if file_name.endswith('.xlsx'):  # Process only Excel files
+        file_path = os.path.join(folder_path, file_name)
+        print(f"Processing file: {file_name}")
+
+      # Load data from the Excel file according to sheet
+      # Regexes for the table specific
+        uds3_pattern = re.compile(r'.*? UDS3 REDCap')
+        uds4_pattern = re.compile(r'.*? UDS4 REDCap')
+      # Get the sheet names
+        excel_file = pd.ExcelFile(file_path)
+        sheet_names = excel_file.sheet_names
+      # Get the regex matches for UDS3 and UDS4 dictionaries
+        uds3_sheet = [name for name in sheet_names if uds3_pattern.match(name)][0]
+        uds4_sheet = [name for name in sheet_names if uds4_pattern.match(name)][0]
+      # Read in the dictionaries
+        uds3_rdcp = pd.read_excel(file_path, sheet_name=uds3_sheet, header=1)  # UDS3 REDCap dictionary
+        uds4_rdcp = pd.read_excel(file_path, sheet_name=uds4_sheet, header=1)  # UDS4 REDCap dictionary
+      # Simple hard name call for the templated mapping rules for processing
+        df = pd.read_excel(file_path, sheet_name='Template Mappings REDCap', header=1)
+
+      # Clean, lower, and rename columns
+        df.columns = df.columns.str.lower()
+        uds4_rdcp.columns = uds4_rdcp.columns.str.lower()
+        uds3_rdcp.columns = uds3_rdcp.columns.str.lower()
+        uds4_rdcp = uds4_rdcp.iloc[:, :12]
+        uds4_rdcp.rename(columns={"uds4 data element name": "uds4 data element"}, inplace=True)
+
+      # Merge template mappings with UDS4 dictionary
+        test = pd.merge(df, uds4_rdcp, on='uds4 data element')
+
+      # Apply classification and categorization of variable pairings
+        result = classify_variable(test)
+      # Ignore any variables that are missing a UDS3/UDS4 neighbor
+        result['uds3 value'] = result['uds3 value'].where(result['uds3 value'].notnull(), None)
+        result['uds4 value'] = result['uds4 value'].where(result['uds4 value'].notnull(), None)
+      # Fill in notes and reversible fields with NA as needed
+        result['notes and discussion points'] = result['notes and discussion points'].fillna("NA")
+        result['reversible to uds3'] = result['reversible to uds3'].fillna("NA")
+      # Recast <BLANK> REDCap LABELS as empty strings
+        result.loc[result['uds4 value'] == "<BLANK>", 'uds4 value'] = ""
+
+      # Apply the primary categorization and JSON-esque dictionary building function
+        all_mappings = categorize_variables(result)
+      # Clean text as described and format as JSON files
+        all_mappings = clean_text(all_mappings)
+        all_mappings = json.dumps(all_mappings, ensure_ascii=False, indent=4,default=json_serializer)
+
+      # Save output to JSON
+        output_path = os.path.join(folder_path, f"{os.path.splitext(file_name)[0]}_mappings.json")
+        with open(output_path, 'w', encoding='utf-8') as json_file:
+            json_file.write(all_mappings)
+      # Sanity tracker
+        print(f"Processed and saved: {output_path}")
